9a8d6f0ca299a34a8f057c860901459fdc0b3141
Bono Cichowski <2206279cichowski@zuyd.nl>
2024-11-12 14:55:15 +0100

    Add project files.

---
+++ DartApp/Models/GameMode.cs
@@ -0,0 +1,20 @@
+﻿using System.Media;
+
+namespace DartApp.Models
+{
+    public abstract class GameMode
+    {
+        public int GameModeId { get; set; }
+        public string Name { get; set; }
+        public List<Player> Players
+        public Player CurrentTurn { get; set; }
+        public bool IsFinished { get; set; }
+
+
+
+        public abstract void StartGame();
+        public abstract void CalculateScore(int playerId, int score);
+        public abstract bool CheckWinCondition();
+        public abstract void SwitchTurn();
+    }
+}
---
+++ DartApp/Models/GameMode301.cs
@@ -0,0 +1,47 @@
+﻿namespace DartApp.Models
+{
+    public class GameMode301 : GameMode
+    {
+        public GameMode301()
+        {
+            GameModeId = 1;
+            Name = "301";
+        }
+
+        public override void StartGame()
+        {
+            foreach (Player player in Players)
+            {
+                player.Score = 301;
+            }
+        }
+
+        public override void CalculateScore(int playerId, int score)
+        {
+            var player = Players.First(p => p.PlayerId == playerId);
+            player.Score -= score;
+            if (player.Score <0)
+            {
+                player.Score += score;
+            }
+        }
+
+        public override bool CheckWinCondition()
+        {
+            return Players.Any(p => p.Score == 0);
+        }
+
+        public override void SwitchTurn()
+        {
+            var index = Players.IndexOf(CurrentTurn);
+            if (index == Players.Count - 1)
+            {
+                CurrentTurn = Players.First();
+            }
+            else
+            {
+                CurrentTurn = Players[index + 1];
+            }
+        }
+    }
+}
