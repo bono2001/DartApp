8e625d6921cd44a680c462f0f78fb6a4bfba1b88
Bono Cichowski <2206279cichowski@zuyd.nl>
2024-11-12 15:05:19 +0100

    Created all Models

---
+++ DartApp/Models/Game.cs
@@ -0,0 +1,30 @@
+﻿namespace DartApp.Models
+{
+    public class Game
+    {
+        public int GameId { get; set; }
+        public GameMode GameMode { get; set; }
+        public List<Player> Players { get; set; }
+
+
+        public Game()
+        {
+            Players = new List<Player>();
+        }
+
+        public void StartGame()
+        {
+            GameMode.StartGame();
+        }
+
+        public void AddThrow(int playerId, int score)
+        {
+           GameMode.CalculateScore(playerId, score);
+        }
+
+        public bool CheckWinCondition()
+        {
+            return GameMode.CheckWinCondition();
+        }
+    }
+}
---
+++ DartApp/Models/Player.cs
@@ -0,0 +1,9 @@
+﻿namespace DartApp.Models
+{
+    public class Player
+    {
+        public int PlayerId { get; set; }
+        public string Name { get; set; }
+        public int Score { get; set; }
+    }
+}
